07ee9f166ee52a55f5b577acd392192fe9348892
Zero-day0001 <35689207+Zero-day0001@users.noreply.github.com>
2022-08-31 18:18:00 -0400

    Add files via upload

--- public/scripts/reboot.sh
+++ public/scripts/reboot.sh
@@ -3,24 +3,30 @@
 IFS=$'\n'
 lanip="$(grep -oE '\$lanip = .*;' config.php | tail -1 | sed 's/$lanip = //g;s/;//g;s/^"//;s/"$//')"
 adbport="$(grep -oE '\$adbport = .*;' config.php | tail -1 | sed 's/$adbport = //g;s/;//g;s/^"//;s/"$//')"
+rm outputs/reboot.log
+exec > outputs/reboot.log 2>&1
 for i in `cat scripts/ips` ; do
   if [[ $i =~ "{".* ]] ; then
     first=$(echo $i | cut -d '.' -f1 | cut -d '{' -f2)
     last=$(echo $i | cut -d '.' -f3 | cut -d '}' -f1)
     for (( j = $first ; j <= $last ; j++ )) ; do
       ip="$lanip.$j"
-      adb start-server > /dev/null 2>&1
-      adb connect $ip:$adbport > /dev/null 2>&1
+      adb start-server
+      adb connect $ip:$adbport
+      echo Rebooting Device on $ip
       sleep 1
-      adb shell reboot > /dev/null 2>&1
-      adb kill-server > /dev/null 2>&1
+      adb shell reboot
+      adb kill-server
     done
   else
     ip="$lanip.$i"
-    adb start-server > /dev/null 2>&1
-    adb connect $ip:$adbport > /dev/null 2>&1 
+    adb start-server
+    adb connect $ip:$adbport
+    echo Rebooting Device on $ip
     sleep 1
-    adb shell reboot > /dev/null 2>&1
-    adb kill-server > /dev/null 2>&1
+    adb shell reboot
+    adb kill-server
   fi
 done
+echo Checking ADB server was killed
+adb kill-server
