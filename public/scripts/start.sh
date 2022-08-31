c7f2dd9423559ab3667befef3f2b3f3c0a110ee9
Zero-day0001 <35689207+Zero-day0001@users.noreply.github.com>
2022-08-31 18:08:41 -0400

    Add files via upload

--- public/scripts/start.sh
+++ public/scripts/start.sh
@@ -3,24 +3,30 @@
 IFS=$'\n'
 lanip="$(grep -oE '\$lanip = .*;' config.php | tail -1 | sed 's/$lanip = //g;s/;//g;s/^"//;s/"$//')"
 adbport="$(grep -oE '\$adbport = .*;' config.php | tail -1 | sed 's/$adbport = //g;s/;//g;s/^"//;s/"$//')"
+rm outputs/startapps.log
+exec > outputs/startapps.log 2>&1
 for i in `cat scripts/ips` ; do
   if [[ $i =~ "{".* ]] ; then
     first=$(echo $i | cut -d '.' -f1 | cut -d '{' -f2)
     last=$(echo $i | cut -d '.' -f3 | cut -d '}' -f1)
     for (( j = $first ; j <= $last ; j++ )) ; do
       ip="$lanip.$j"
-      adb start-server > /dev/null 2>&1
-      adb connect $ip:$adbport > /dev/null 2>&1
+      adb start-server
+      adb connect $ip:$adbport
+      echo Starting Atlas Service on device - $ip
       sleep 1
-      adb shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService" > /dev/null 2>&1
-      adb kill-server > /dev/null 2>&1
+      adb shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
+      adb kill-server
     done
   else
     ip="$lanip.$i"
-    adb start-server > /dev/null 2>&1
-    adb connect $ip:$adbport > /dev/null 2>&1 
+    adb start-server
+    adb connect $ip:$adbport
+    echo Starting Atlas Service on device - $ip
     sleep 1
-    adb shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService" > /dev/null 2>&1
-    adb kill-server > /dev/null 2>&1
+    adb shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
+    adb kill-server
   fi
 done
+echo Checking ADB server was killed
+adb kill-server
--- public/scripts/uppogo.sh
+++ public/scripts/uppogo.sh
@@ -3,8 +3,8 @@
 IFS=$'\n'
 lanip="$(grep -oE '\$lanip = .*;' config.php | tail -1 | sed 's/$lanip = //g;s/;//g;s/^"//;s/"$//')"
 adbport="$(grep -oE '\$adbport = .*;' config.php | tail -1 | sed 's/$adbport = //g;s/;//g;s/^"//;s/"$//')"
-rm outputs/stopapps.log
-exec > outputs/stopapps.log 2>&1
+rm outputs/update-pogo.log
+exec > outputs/update-pogo.log 2>&1
 for i in `cat scripts/ips` ; do
   if [[ $i =~ "{".* ]] ; then
     first=$(echo $i | cut -d '.' -f1 | cut -d '{' -f2)
@@ -13,18 +13,20 @@ for i in `cat scripts/ips` ; do
       ip="$lanip.$j"
       adb start-server
       adb connect $ip:$adbport
+      echo Updating Pokemon on $ip
       sleep 1
-      echo Stopping Pokemon and Atlas Services on - $ip
-      adb shell "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas"
+      adb install -r app/pokemongo.apk
+      adb install -r app/atlas.apk
       adb kill-server
     done
   else
     ip="$lanip.$i"
     adb start-server
     adb connect $ip:$adbport
+    echo Updating Pokemon on $ip
     sleep 1
-    echo Stopping Pokemon and Atlas Services on - $ip
-    adb shell "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas"
+    adb install -r app/pokemongo.apk
+    adb install -r app/atlas.apk
     adb kill-server
   fi
 done
