6e9ba06f455ae20ab4481a025fc7735a587bbfdc
Zero-day0001 <35689207+Zero-day0001@users.noreply.github.com>
2022-08-31 00:34:22 -0400

    Add files via upload

--- public/header.php
+++ public/header.php
@@ -4,24 +4,26 @@ require('sections.php');
 require('functions.php');
 require('footer.php');
 
-function Menu() { ?>
+function Menu() { 
 	
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/style.css">
-</head>
+echo '<head>' .
+'<meta name="viewport" content="width=device-width, initial-scale=1">' .
+'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">' .
+'<link rel="stylesheet" href="css/style.css">' .
+'</head>' .
 
-<header>
-<div class="topnav" id="TopnavMenu">
-  <a href="#" class="logo">ATV Controller</a>
-  <a href="index.php" class="active">Home</a>
-  <a href="javascript:void(0);" class="icon" onclick="menu()">
-    <i class="fa fa-bars"></i>
-  </a>
-</div>
-</header>
+'<header>'.
 
-<?php } ?>
+'<div class="topnav" id="TopnavMenu">' .
+  '<a href="#" class="logo">ATV Controller</a>' .
+  '<a href="index.php" class="active">Home</a>' .
+  '<a href="javascript:void(0);" class="icon" onclick="menu()">' .
+    '<i class="fa fa-bars"></i>' .
+  '</a>' .
+'</div>' .
 
-<body>
+'</header>' . 
+
+'<body>'; 
+ } ?>
+
