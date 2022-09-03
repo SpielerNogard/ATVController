d1647d02748712cefc421e0bd25341cd20e12d8e
xxleevo <przylutzkimatthias@gmail.com>
2022-09-03 15:49:00 +0200

    Close Tab if pressed in active state

--- public/functions.php
+++ public/functions.php
@@ -400,17 +400,22 @@ echo '<div class="cssContainer">' .
 
 				<script>
 					function openTab(evt, tabName, deviceName) {
-					var i, tabcontent, tablinks;
-					tabcontent = document.getElementsByClassName("tabcontent-" + deviceName);
-					for (i = 0; i < tabcontent.length; i++) {
-						tabcontent[i].style.display = "none";
-					}
-					tablinks = document.getElementsByClassName("tablinks-" + deviceName);
-					for (i = 0; i < tablinks.length; i++) {
-						tablinks[i].className = tablinks[i].className.replace(" active", "");
-					}
-					document.getElementById(tabName).style.display = "block";
-					evt.currentTarget.className += " active";
+						var i, tabcontent, tablinks;
+						if(evt.currentTarget.classList.contains("active")){
+							document.getElementById(tabName).style.display = "none";
+							document.getElementById(tabName).className.replace(" active", "");
+						}else {
+							tabcontent = document.getElementsByClassName("tabcontent-" + deviceName);
+							for (i = 0; i < tabcontent.length; i++) {
+								tabcontent[i].style.display = "none";
+							}
+							tablinks = document.getElementsByClassName("tablinks-" + deviceName);
+							for (i = 0; i < tablinks.length; i++) {
+								tablinks[i].className = tablinks[i].className.replace(" active", "");
+							}
+							document.getElementById(tabName).style.display = "block";
+							evt.currentTarget.className += " active";
+						}
 					}
 					
 					// Get the element with id="defaultOpen" and click on it
